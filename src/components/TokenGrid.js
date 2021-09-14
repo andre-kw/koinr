91e6a95c8a256872cb6c2e2e2f8f551bc2521376
Andre K W <andre.k.willie@gmail.com>
2021-09-14 00:01:44 -0400

    sugar and spice

--- src/components/TokenGrid.js
+++ src/components/TokenGrid.js
@@ -1,11 +1,13 @@
 import React, {useState} from 'react';
 import {Contract} from '@ethersproject/contracts';
+import PullToRefresh from 'pulltorefreshjs';
 import AccountContext from 'contexts/AccountContext';
 import useWallet from '../hooks/Wallet';
 import useErrorHandler from '../hooks/ErrorHandler';
 import TokenButton from './TokenButton';
 import bscscan from '../apis/bscscan';
 import abi from '../abi';
+import {pullToReleaseConfig} from '../config';
 import './styles/Tokens.css';
 
 export default function TokenGrid(props) {
@@ -14,66 +16,80 @@ export default function TokenGrid(props) {
   const handleError = useErrorHandler();
   const [loading, setLoading] = useState(true);
 
-  React.useEffect(() => {
-    const txnIterator = async (txs) => {
-      return {
-        async *[Symbol.asyncIterator]() {
-          for(let i = 0; i < txs.length; i++) {
-            if(txs.txreceipt_status !== "1")
-              yield;
+  const txnIterator = async (txs) => {
+    return {
+      async *[Symbol.asyncIterator]() {
+        for(let i = 0; i < txs.length; i++) {
+          if(txs.txreceipt_status !== "1")
+            yield;
 
-            if(txs[i].from === eth.selectedAddress()) {
-              // sending/swapping
-              let code;
+          if(txs[i].from === eth.selectedAddress()) {
+            // sending/swapping
+            let code;
 
-              try {
-                code = await eth.getCode(txs[i].to);
-              } catch(e) {
-                handleError(e);
-              }
-
-              if(code && code !== '0x')
-                yield {address: txs[i].to};
-
-            } else if(txs[i].to === eth.selectedAddress()) {
-              // TODO: receiving
+            try {
+              code = await eth.getCode(txs[i].to);
+            } catch(e) {
+              handleError(e);
             }
+
+            if(code && code !== '0x')
+              yield {address: txs[i].to};
+
+          } else if(txs[i].to === eth.selectedAddress()) {
+            // TODO: receiving
           }
         }
-      };
+      }
     };
+  };
 
-    (async () => {
-      let txs;
-      const temp = [];
+  const loadTokens = async () => {
+    let txs;
+    const temp = [];
+
+    setLoading(true);
+
+    try {
+      txs = await bscscan.txlist(eth.selectedAddress());
+    } catch(e) {
+      setLoading(false);
+      handleError(e);
+    }
+
+    for await (let token of await txnIterator(txs)) {
+      if(!token || temp.findIndex(t => token.address === t.address) !== -1)
+        continue;
+
+      let contract, name, symbol;
 
       try {
-        txs = await bscscan.txlist(eth.selectedAddress());
+        contract = new Contract(token.address, abi, eth.provider);
+        name = await contract.name();
+        symbol = await contract.symbol();
       } catch(e) {
         handleError(e);
       }
+      
+      temp.push({...token, contract, name, symbol});
+    }
 
-      for await (let token of await txnIterator(txs)) {
-        if(!token || temp.findIndex(t => token.address === t.address) !== -1)
-          continue;
+    acc.setTxs([...txs]);
+    acc.setTokens([...temp]);
+    setLoading(false);
+  };
 
-        let contract, name, symbol;
+  React.useEffect(() => {
+    PullToRefresh.init({
+      ...pullToReleaseConfig,
+      onRefresh: () => loadTokens()
+    });
 
-        try {
-          contract = new Contract(token.address, abi, eth.provider);
-          name = await contract.name();
-          symbol = await contract.symbol();
-        } catch(e) {
-          handleError(e);
-        }
-        
-        temp.push({...token, contract, name, symbol});
-      }
+    loadTokens();
 
-      acc.setTxs([...txs]);
-      acc.setTokens([...temp]);
-      setLoading(false);
-    })();
+    return function cleanup() {
+      PullToRefresh.destroyAll();
+    };
   }, []);
 
   const openInfoDrawer = (address) => {
@@ -83,8 +99,8 @@ export default function TokenGrid(props) {
   return (
     <section id="tokens" className={loading ? 'loading' : ''}>
       <div>
-        {loading && <p>loading...</p>}
-        {acc.tokens.map(token => 
+        {loading && <div id="load-spinner" role="alert" aria-live="polite" aria-label="loading"></div>}
+        {!loading && acc.tokens.map(token => 
           <TokenButton key={token.address} token={token} onClick={() => openInfoDrawer(token.address)} />)}
       </div>
     </section>
---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const pullToReleaseConfig = {
+  mainElement: 'body',
+  triggerElement: '#tokens > div',
+  instructionsPullToRefresh: 'Pull to refresh',
+  resistanceFunction: t => Math.min(1, t / 2.75),
+  // distIgnore: 20,
+  distThreshold: 50,
+  distReload: 60,
+  instructionsRefreshing: ' '
+};
